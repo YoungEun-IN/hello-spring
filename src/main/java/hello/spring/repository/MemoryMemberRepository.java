24a63dc2634d59b50124ef3c7b5364412435aaba
yyoungeunin <yyoungeunin@gmail.com>
2022-03-09 17:30:29 +0900

    회원 도메인과 리포지토리, 테스트케이스 작성

---
+++ src/main/java/hello/spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.spring.repository;
+
+import hello.spring.domain.Member;
+
+import java.util.*;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> storage = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        storage.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(storage.get(id));
+    }
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        return storage.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+    public void clearStorage() {
+        storage.clear();
+    }
+}
