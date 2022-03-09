e55fa87767500f860b845fa345a87f4dae47a36f
yyoungeunin <yyoungeunin@gmail.com>
2022-03-09 22:58:05 +0900

    JPA

--- src/main/java/hello/spring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/spring/repository/JdbcTemplateMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -13,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
 public class JdbcTemplateMemberRepository implements MemberRepository {
     private final JdbcTemplate jdbcTemplate;
 
--- src/main/java/hello/spring/service/MemberService.java
+++ src/main/java/hello/spring/service/MemberService.java
@@ -4,11 +4,13 @@ import hello.spring.domain.Member;
 import hello.spring.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
+@Transactional
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
